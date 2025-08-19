03645c04e936f1b075d7760e9d052327d6fd938b
iqinzhe <iqinzhe@gmail.com>
2025-08-19 14:16:46 +0700

    Add files via upload

---
+++ events.js
@@ -0,0 +1,22 @@
+const events = [
+  {
+    id: 1,
+    title: "自然扭曲琴颈与胴身系列发布",
+    date: "2025-08-15",
+    location: "北京123 AX加工中心",
+    image: "images/example-1.jpg",
+    description: "我们的自然扭曲设计能够与人体实现更无缝的交互。其符合人体工程学的设计提高了手部效率，降低劳损风险。",
+    cta: "预约参加发布会",
+    isNew: true
+  },
+  {
+    id: 2,
+    title: "专业品质。经典 AX设计。",
+    date: "2025-06-15",
+    location: "上海567 AX加工中心",
+    image: "images/example-2.jpg",
+    description: "AX E series 新增了绗缝枫木面板和琴头，赋予舞台风格和可靠品质，配备24品、34英寸弦长与双线圈拾音器。",
+    cta: "预约参加发布会",
+    isNew: false
+  }
+];
